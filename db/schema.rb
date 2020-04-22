e5271f4468539a31f090edf8b859da2877dde9e5
anthonyoleary <anthony.oleary@finddx.org>
2020-04-22 10:12:47 +0100

    fixed rebase issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_113252) do
+ActiveRecord::Schema.define(version: 2020_04_21_100958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -36,14 +36,6 @@ ActiveRecord::Schema.define(version: 2020_04_20_113252) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "eurostat_nuts_regions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "postcode", limit: 20
-    t.string "code", limit: 20
-    t.datetime "created_at", default: -> { "now()" }, null: false
-    t.datetime "updated_at", default: -> { "now()" }, null: false
-    t.index ["postcode"], name: "index_eurostat_nuts_regions_on_postcode", unique: true
   end
 
   create_table "facilities_management_buildings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -301,7 +293,6 @@ ActiveRecord::Schema.define(version: 2020_04_20_113252) do
     t.boolean "direct_award"
     t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }
     t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }
-    t.index ["facilities_management_procurement_id"], name: "index_frozen_fm_rates_procurement"
   end
 
   create_table "fm_lifts", id: false, force: :cascade do |t|
@@ -513,12 +504,6 @@ ActiveRecord::Schema.define(version: 2020_04_20_113252) do
     t.index ["code"], name: "nuts_regions_code_key", unique: true
   end
 
-  create_table "ons_nuts3_regions", id: false, force: :cascade do |t|
-    t.string "NUTS318CD", limit: 255
-    t.string "NUTS318NM", limit: 255
-    t.integer "FID"
-  end
-
   create_table "os_address", id: false, force: :cascade do |t|
     t.bigint "uprn", null: false
     t.bigint "udprn"
@@ -614,8 +599,8 @@ ActiveRecord::Schema.define(version: 2020_04_20_113252) do
   create_table "postcodes_nuts_regions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "postcode", limit: 20
     t.string "code", limit: 20
-    t.datetime "created_at", default: -> { "now()" }, null: false
-    t.datetime "updated_at", default: -> { "now()" }, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["postcode"], name: "index_postcodes_nuts_regions_on_postcode", unique: true
   end
 
