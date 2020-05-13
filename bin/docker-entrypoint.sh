a86ce16eb5c2253a19968a3a8ac256710cdf63c7
Iulia R Ionita <iulia@ionita.me>
2020-05-13 10:37:45 +0100

    Updated docker-entrypoints

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
-echo TCPAddr $CLAMAV_SERVER_IP > /etc/clamav/clamd.conf && echo TCPSocket 3310 >> /etc/clamav/clamd.conf
-
-bundle exec rails db:migrate
-
-if [ "$APP_RUN_SIDEKIQ" = 'TRUE' ]; then
-  bundle exec sidekiq -C ./config/sidekiq.yml -d -L ./log/sidekiq.log -e production
-fi
-
-if [ "$APP_RUN_STATIC_TASK" = 'TRUE' ]; then
-  bundle exec rails db:static
-fi
-
-if [ "$APP_RUN_POSTCODES_IMPORT" = 'TRUE' ]; then
-  bundle exec rails db:postcode
-fi
-
-if [ "$APP_RUN_NUTS_IMPORT" = 'TRUE' ]; then
-  bundle exec rails db:run_postcodes_to_nuts_worker
-fi
-
-bundle exec rails server
+#echo TCPAddr $CLAMAV_SERVER_IP > /etc/clamav/clamd.conf && echo TCPSocket 3310 >> /etc/clamav/clamd.conf
+#
+#bundle exec rails db:migrate
+#
+bundle exec sidekiq -C ./config/sidekiq.yml -e production
+#
+#if [ "$APP_RUN_STATIC_TASK" = 'TRUE' ]; then
+#  bundle exec rails db:static
+#fi
+#
+#if [ "$APP_RUN_POSTCODES_IMPORT" = 'TRUE' ]; then
+#  bundle exec rails db:postcode
+#fi
+#
+#if [ "$APP_RUN_NUTS_IMPORT" = 'TRUE' ]; then
+#  bundle exec rails db:run_postcodes_to_nuts_worker
+#fi
+#
+#bundle exec rails server
